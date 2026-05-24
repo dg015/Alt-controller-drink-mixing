cf98c989688f6b65f2e9656791bc2b5c4c2b1bd0
dg015 <bruno.barbosa8@yahoo.com>
2026-05-24 14:57:32 -0400

    Main menu and credits scene set up

--- Drink Mixing alt ctrl/Assets/Scripts/Player/Player.cs
+++ Drink Mixing alt ctrl/Assets/Scripts/Player/Player.cs
@@ -3,16 +3,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-
     [SerializeField] private List <Ingredients> m_currentIngredients = new List <Ingredients> ();
     [SerializeField] private int m_numOfIngredientsPerDrink;
 
 
-
+    private string previousBottleID;
 
     public void EmptyCup()
     {
         m_currentIngredients.Clear ();
     }
+
+    private void addToCup()
+    {
+       //check if the data has been updated
+        if (ArduinoDataReceiver.Instance.bottleData != previousBottleID)
+        {
+            //call here function to add to cup based on the ID of the tag
+            //Manager.Instance.
+        }
+
+        previousBottleID = ArduinoDataReceiver.Instance.bottleData;
+    }
 }
+
