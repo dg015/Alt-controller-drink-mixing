aadaf2dbfab9afb3214f2fd16dd71037886c0d5a
dg015 <bruno.barbosa8@yahoo.com>
2026-05-27 10:01:31 -0400

    cup now has a UI element of its own
    cup UI shows which ingredients are in the cup

--- Drink Mixing alt ctrl/Assets/Scripts/Player/playerCupUIUpdater.cs
+++ Drink Mixing alt ctrl/Assets/Scripts/Player/playerCupUIUpdater.cs
@@ -1,34 +1,39 @@
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class ClientUIUpdater : MonoBehaviour
+public class playerCupUIUpdater : MonoBehaviour
 {
-    [SerializeField] private Client client;
+    [SerializeField] private Player playerScript;
     [SerializeField] private List<Image> UIImages;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        updateUi();
+        
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        updateUi();
+    }
     private void updateUi()
     {
-        for (int i  = 0; i < client.order.Count;i++)
+        for (int i = 0; i < playerScript.currentIngredients.Count; i++)
         {
-            if(client.order[i] == Ingredients.Red)
+            if (playerScript.currentIngredients[i] == Ingredients.Red)
             {
                 UIImages[i].color = Color.red;
             }
-            else if (client.order[i] == Ingredients.Green)
+            else if (playerScript.currentIngredients[i] == Ingredients.Green)
             {
                 UIImages[i].color = Color.green;
             }
-            else if (client.order[i] == Ingredients.Blue)
+            else if (playerScript.currentIngredients[i] == Ingredients.Blue)
             {
                 UIImages[i].color = Color.blue;
             }
-            else if (client.order[i] == Ingredients.White)
+            else if (playerScript.currentIngredients[i] == Ingredients.White)
             {
                 UIImages[i].color = Color.white;
             }
